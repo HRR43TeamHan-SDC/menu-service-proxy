feaf7420201ee50eb68f0ed4ef7e8e7399a4efc0
Jordan Heskett <jordanheskett@gmail.com>
2020-02-23 15:37:54 -0600

    Using dotenv to ease environment setup in production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
+//
+require('dotenv').config();
 
-app.use(express.static('public'));
+const PROXY_PORT = process.env.PROXY_PORT || 8080;
+const MENU_HOSTNAME = process.env.MENU_HOSTNAME || 'localhost';
+const MENU_PORT = process.env.MENU_PORT || 8001;
 
-const port = 3043;
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+
+app.get('/gettitle/:id', (req, res) => {
+  axios.get(`http://${MENU_HOSTNAME}:${MENU_PORT}${req.url}`)
+  .then(response => response.data)
+  .then(data => res.send({data}))
+  .catch(err => console.log('error at proxy serving',err));
+});
+
+app.get('/getmenu/:id', (req, res) => {
+  axios.get(`http://${MENU_HOSTNAME}:${MENU_PORT}${req.url}`)
+  .then(response => response.data)
+  .then(data => res.send(data))
+  .catch(err => console.log('error at proxy serving',err));
+});
+
+
+app.use('/:id',express.static('public'));
+
+
+app.listen(PROXY_PORT, () => {
+  console.log(`App listening on port ${PROXY_PORT}`);
 });
